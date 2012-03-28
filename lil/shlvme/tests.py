53e665a47fced79211f4e639a0b4644cc698ad2c
Matt Phillips <mphillips@law.harvard.edu>
2012-03-27 21:24:40 -0400

    added logic to handle bookmarklet incoming from imdb stubbed out logic to handle bookmarklet incoming from amazon (not done yet) added test cases for imdb and amazon incoming logic

--- lil/shlvme/tests.py
+++ lil/shlvme/tests.py
@@ -10,7 +10,8 @@ from django.utils import unittest
 from django.contrib.auth.models import User
 from django.test.client import Client
 from django.core.urlresolvers import reverse
-from lil.shlvme.models import Shelf, Item, Creator, Tag        
+from django.test.client import RequestFactory
+from lil.shlvme.models import Shelf, Item, Creator, Tag 
 
 class MacroTestCase(unittest.TestCase):
     def setUp(self):
@@ -43,3 +44,32 @@ class MacroTestCase(unittest.TestCase):
         
         # Add a tag to the item
         response = self.client.post(reverse('api_tag'), {'item-uuid': item.item_uuid, 'tag-key': 'test-tag-key', 'tag-value': 'test-tag-value'})
+        
+        
+class ImdbTestCase(unittest.TestCase):
+    """Test our IMDB bookmarklet functionality. We'll depend on the MacroTestCase to
+    set things up properly.
+    """
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        
+    def tearDown(self):
+        pass
+
+    def test_add_imdb_item(self):
+        response = self.client.get(('%s?loc=http://www.imdb.com/title/tt0109686/' % reverse('incoming')))
+        self.assertEqual('Dumb & Dumber', response.context['title'])
+
+class AmazonTestCase(unittest.TestCase):
+    """Test our Amazon bookmarklet functionality. We'll depend on the MacroTestCase to
+    set things up properly.
+    """
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        
+    def tearDown(self):
+        pass
+
+    def test_add_imdb_item(self):
+        response = self.client.get(('%s?loc=http://www.amazon.com/Blood-Meridian-Evening-Redness-West/dp/0679728759/ref=sr_1_1?s=books&ie=UTF8&qid=1332896433&sr=1-1' % reverse('incoming')))
+        self.assertEqual('Blood Meridian: Or the Evening Redness in the West', response.context['title'])
--- lil/shlvme/views/services.py
+++ lil/shlvme/views/services.py
@@ -1,30 +1,92 @@
 from django.shortcuts import render_to_response
 from amazonproduct import API
+import re
+import logging
+import urllib
+import urllib2
+import json
+from lxml.etree import tostring
+
+try:
+    from lil.shlvme.local_settings import *
+except ImportError, e:
+    print 'Unable to load local_settings.py:', e
+    
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s %(levelname)s %(filename)s %(lineno)d %(message)s',
+    filename = '/tmp/shlvme.log',
+    filemode = 'w'
+)
+
 
 def incoming(request):
-    """We have an incoming item (probably from the bookmarklet)"""
-    
-    #TODO: this is nothing more than a test now. cleanup.
     url = request.GET.get('loc', None)
+    
+    if re.search(r'imdb\.com', url):
+        details = get_imdb_details(url)
+        return render_to_response('add-item.html', {'user': request.user, 'title': details['title'],
+                                                    'link': url, 'content_type': details['content_type'],
+                                                    'creator': details['creator'], 'pub_date': details['pub_date']})
+    elif re.search(r'amazon\.com', url):
+        details = get_amazon_details(url)
+        return render_to_response('add-item.html', {'user': request.user, 'title': details['title'],
+                                                    'link': url, 'creator': details['creator']})
+
+def get_amazon_details(url):
+    """Given an IMDB URL, get title, creator, etc. from imdapi.com
+    """
     matches = re.search(r'\/([A-Z0-9]{10})($|\/)', url)
     asin = matches.group(1)
-    
-    aws_key = AMZ.KEY 
-    aws_secret_key = AMZ.SECRET_KEY 
+
+    aws_key = AMZ['KEY']
+    aws_secret_key = AMZ['SECRET_KEY']
     api = API(aws_key, aws_secret_key, 'us')
     
-    for root in api.item_lookup(asin, IdType='ASIN', AssociateTag= AMZ.ASSOCIATE_TAG):
+    details = {}
+    
+    for root in api.item_lookup(asin, IdType='ASIN', AssociateTag= AMZ['ASSOCIATE_TAG'], ResponseGroup='Large'):
+        logger.info( tostring(root, pretty_print=True))
         nspace = root.nsmap.get(None, '')
         amazon_items = root.xpath('//aws:Items/aws:Item', namespaces={'aws' : nspace})
-        author = u'Unknown'
-        title = u'Unknown'
-        isbn = u'Unknown'
 
         for amazon_item in amazon_items:
+            #logger.info(dir(amazon_item['ItemAttributes']))
             if hasattr(amazon_item.ItemAttributes, 'Author'): 
-                author = unicode(amazon_item.ItemAttributes.Author)
+                details['creator'] = unicode(amazon_item.ItemAttributes.Author)
 
             if hasattr(amazon_item.ItemAttributes, 'Title'): 
-                title = unicode(amazon_item.ItemAttributes.Title)
+                details['title'] = unicode(amazon_item.ItemAttributes.Title)
+
+    details['link'] = url
+    #details['content_type'] = 'dvd'
+    #details['pub_date'] = response_json['Year']
     
-    return render_to_response('add-item.html', {'user': request.user, 'creator': author, 'title': title, 'isbn': isbn})
+    return details
+    
+
+def get_imdb_details(url):
+    """Given an IMDB URL, get title, creator, etc. from imdapi.com
+    """
+    matches = re.search(r'/([a-zA-Z]{2}[0-9]{7})', url)
+    imdb_id = matches.group(1)
+    
+    details = {}
+
+    url = 'http://www.imdbapi.com/'
+    url_params = {'i' : imdb_id}
+
+    data = urllib.urlencode(url_params)
+    url = url + '?' + data
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    response_json = json.loads(response.read())
+
+    details['title'] = response_json['Title']
+    details['link'] = url
+    details['content_type'] = 'dvd'
+    details['creator'] = response_json['Director']
+    details['pub_date'] = response_json['Year']
+    
+    return details
