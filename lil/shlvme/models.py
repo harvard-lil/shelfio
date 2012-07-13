b120bac8487b993e8a4bf51d6cc167f5a60d89c0
Matt Phillips <mphillips@law.harvard.edu>
2012-07-13 17:05:22 -0400

    default url field lenth is 200 chars? WTF, Django? Bumped to 2000 chars. fixes #90

--- lil/shlvme/models.py
+++ lil/shlvme/models.py
@@ -41,7 +41,7 @@ class Item(models.Model):
     shelf = models.ForeignKey(Shelf)
     item_uuid = UUIDField(auto=True)
     title = models.CharField(max_length=200)
-    link = models.URLField()
+    link = models.URLField(max_length=2000)
     measurement_page_numeric = models.PositiveIntegerField(default=300)
     measurement_height_numeric = models.DecimalField(default='25.5', max_digits=5, decimal_places=2)
     format = models.CharField(max_length=200, default='book')
