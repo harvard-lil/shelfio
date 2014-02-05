cdb63bdc57a50f1e33718322a9d2c9bd632ea0e3
Matt Phillips <mphillips@law.harvard.edu>
2014-02-05 14:05:04 -0500

    freshened up settings file

--- lil/settings.example.py
+++ lil/settings.example.py
@@ -1,7 +1,24 @@
-# Django settings for lil project.
+import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'your_value',                      # Or path to database file if using sqlite3.
+        'USER': 'your_value',
+        'PASSWORD': 'your_value',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
+        'OPTIONS': {
+            "init_command": "SET storage_engine=INNODB; SET foreign_key_checks = 0; SET NAMES 'utf8';",
+            "charset": "utf8",
+        },
+    }
+}
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -9,24 +26,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dbname',                      # Or path to database file if using sqlite3.
-        'USER': 'dbuser',                      # Not used with sqlite3.
-        'PASSWORD': 'dbpass',                  # Not used with sqlite3.
-        'HOST': 'dbhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'your key here'
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+# To populate the from field of emails sent from the project
+DEFAULT_FROM_EMAIL = 'email@example.com'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
@@ -40,35 +53,30 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
 
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
+# Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
 
 # URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    'static',
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -77,9 +85,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'yoursecretkeyhere'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -94,15 +99,22 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'lil.shelfio.middleware.MethodOverrideMiddleware',
+    'ratelimit.middleware.RatelimitMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'lil.urls'
+ROOT_URLCONF = 'shelfio.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
 )
 
 INSTALLED_APPS = (
@@ -112,11 +124,57 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lil.shelfio',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+)
+
+LOGIN_REDIRECT_URL = '/manage/create/'
+LOGIN_URL = '/login'
+
+# Broker used by celery
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
+
+# When getting the source with wget, let's set some details
+ARCHIVE_QUOTA = '20m' # Maximum filesize
+ARCHIVE_LIMIT_RATE = '100m' # Download limit rate; TODO reduce for production
+ACCEPT_CONTENT_TYPES = [ # HTTP content-type parameters to accept
+    'text/html',
+    'text/xml',
+    'application/xhtml+xml',
+    'application/xml'
+]
+NUMBER_RETRIES = 3 # if wget fails to get a resource, try to get again this many times
+WAIT_BETWEEN_TRIES = .5 # wait between .5 and this many seconds between http requests to our source
+
+# Max file size (for our downloads)
+MAX_ARCHIVE_FILE_SIZE = 1024 * 1024 * 100 # 100 MB
+
+# Rate limits
+MINUTE_LIMIT = '6000/m'
+HOUR_LIMIT = '100000/h'
+DAY_LIMIT = '500000/d'
+REGISTER_MINUTE_LIMIT = '600/m'
+REGISTER_HOUR_LIMIT = '2000/h'
+REGISTER_DAY_LIMIT = '5000/d'
+LOGIN_MINUTE_LIMIT = '5000/m'
+LOGIN_HOUR_LIMIT = '10000/h'
+LOGIN_DAY_LIMIT = '50000/d'
+
+# Dashboard user lists
+MAX_USER_LIST_SIZE = 100
+
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    'static',
+
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 # A sample logging configuration. The only tangible logging
@@ -132,9 +190,15 @@ LOGGING = {
             'format': '%(asctime)s [%(levelname)s] %(filename)s %(lineno)d: %(message)s'
         },
     },
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+     },
     'handlers': {
         'default': {
             'level':'INFO',
+            'filters': ['require_debug_false'],
             'class':'logging.handlers.RotatingFileHandler',
             'filename': '/tmp/shelfio.log',
             'maxBytes': 1024*1024*5, # 5 MB
@@ -143,6 +207,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
@@ -159,16 +224,3 @@ LOGGING = {
         },
     }
 }
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-LOGIN_URL = '/login'
-LOGIN_REDIRECT_URL = '/'
-
-# Email business
-EMAIL_HOST = "localhost"
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = "testing@example.com"
