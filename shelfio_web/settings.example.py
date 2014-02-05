ecb095b723795a8d756c9c4b3a70c393eae79d3a
Matt Phillips <mphillips@law.harvard.edu>
2014-02-05 14:09:18 -0500

    more refactoring of layout to follow the layout we've been using recently in other projects

--- shelfio_web/settings.example.py
+++ shelfio_web/settings.example.py
@@ -224,3 +224,33 @@ LOGGING = {
         },
     }
 }
+
+# Keys
+AMZ = {
+    'KEY': 'youramazonkey',
+    'SECRET_KEY': 'youramazonsecretkey',
+    'ASSOCIATE_TAG': 'yourassociatetag',
+}
+
+GOODREADS = {
+    'KEY': 'yourgoodreadskey'
+}
+
+LIBRARYTHING = {
+    'KEY': 'yourlibrarythingkey'
+}
+
+WORLDCAT = {
+    'KEY': 'yourlibrarythingkey'
+}
+
+# elasticsearch location
+ELASTICSEARCH = {
+    'HOST': 'http://eshost.yourorg.com:9200/shelfio/'
+}
+
+# The location of our api. If in dev, it's probably something like /api/v1/
+# in prod, it's probably something like api.shelf.io/v1/
+SHELFIO_API = {
+    'LOCATION': '/api/v1',
+}
